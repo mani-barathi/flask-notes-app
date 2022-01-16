a37cf99a39dca0dde5015fd617d68ec0f3392643
Manibarathi <49336839+mani-barathi@users.noreply.github.com>
2022-01-16 17:34:57 +0530

    added edit and deelte route

--- main.py
+++ main.py
@@ -15,8 +15,8 @@ def home_page():
     print(notes)
     return render_template('home.html', notes=notes)
 
-# '/edit/23'
-# '/delete/23'
+# '/notes/edit/23'
+# '/notes/delete/23'
 
 
 @app.route("/create", methods=['GET', 'POST'])
@@ -28,6 +28,37 @@ def create_note_page():
     content = request.form.get('content')
     cursor.execute(
         'insert into notes(title, content) values(?,?)', (title,  content))
+    con.commit()
+    return redirect('/')
+
+
+@app.route('/notes/<note_id>')
+def single_note_page(note_id):
+    res = cursor.execute('select * from notes where id=?', (note_id,))
+    note = res.fetchone()
+    print(note)
+    return render_template('single_note.html', note=note)
+
+
+@app.route('/notes/edit/<note_id>', methods=['GET', 'POST'])
+def edit_note_page(note_id):
+    if request.method == 'GET':
+        res = cursor.execute('select * from notes where id=?', (note_id,))
+        note = res.fetchone()
+        return render_template('edit_note.html', note=note)
+
+    title = request.form.get('title')
+    content = request.form.get('content')
+    print('update values', title, content)
+    cursor.execute(
+        'update notes set title=?, content=? where id=?', (title,  content, note_id))
+    con.commit()
+    return redirect(f'/notes/{note_id}')
+
+
+@app.route('/notes/delete/<note_id>')
+def delete_note_page(note_id):
+    cursor.execute('delete from notes where id=?', (note_id,))
     con.commit()
     return redirect('/')
 
